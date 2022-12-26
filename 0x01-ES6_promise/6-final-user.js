9218c681664c8f572b1f520bb0ef1cf357783dd3
Zandarh <ubaksxander@gmail.com>
2022-12-26 13:51:12 +0000

    task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,4 @@
-import signUpUser fron './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
@@ -7,10 +7,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
     const result = [];
+    values.forEach((element) => {
       if (element.status === 'fulfilled') {
         result.push({ status: element.status, value: element.value });
       } else {
-	result.push({ status: element.status, value: `${element.reason}` });
+        result.push({ status: element.status, value: `${element.reason}` });
       }
+    });
+    return result;
   });
-}  
+}
